763a544eb90de66c4bc2274d044b13b2f5c2fa22
Junaid Jarral <jarraljunaid4@gmail.com>
2026-03-19 01:32:39 +0000

    QRQR

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ampoulex-secret-key-2024-pakistan'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///ampoulex.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     PERMANENT_SESSION_LIFETIME = timedelta(days=7)
     
