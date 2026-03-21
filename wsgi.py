862d76b8dc92561d8796870ed8998ef1d9a396fa
Junaid Jarral <jarraljunaid4@gmail.com>
2026-03-21 03:35:45 +0000

    OUI

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,6 @@ from app import create_app
 application = create_app()
 
 if __name__ == '__main__':
-    # Cloud Run sets the PORT environment variable automatically
     import os
     port = int(os.environ.get('PORT', 8080))
     application.run(host='0.0.0.0', port=port, debug=False)
